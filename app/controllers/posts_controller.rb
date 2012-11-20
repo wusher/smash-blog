6181e110a56b50995871d00e42ced22b3662a63e
Will Usher <willusher+github@gmail.com>
2012-11-20 11:37:19 -0800

    minor changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,16 @@
 class PostsController < ApplicationController
+  respond_to :html, :json
 
   before_filter :load_post, :only => [:show]
 
 
   def index
     @posts = Post.all
-
+    respond_with @posts
   end
 
   def show
-
+    respond_with @post
   end
 
   private
