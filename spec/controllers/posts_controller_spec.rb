a842f54434465cd8c4c22aa1b0f916049bede90a
Will Usher <willusher+github@gmail.com>
2012-11-24 09:33:35 -0800

    updated post#index tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,25 +3,35 @@ require 'spec_helper'
 
 describe PostsController do
   describe "#index" do
-    it "works" do
-      posts = [ stub_model(Post), stub_model(Post)]
-      Post.stub(:all).and_return(posts)
-      get :index
-
-      response.code.should == "200"
-      response.should render_template(:index)
-      assigns(:posts).should == posts
+    context "admin is logged in" do
+      Given(:all_posts) { [ stub_model(Post), stub_model(Post)] } 
+      Given { Post.stub(:all).and_return(all_posts) }
+      Given { subject.stub(:current_admin).and_return(stub_model(Admin)) }
+      When { get :index }  
+      Then { response.code.should == "200" }
+      Then { response.should render_template(:index) }
+      Then { assigns(:posts).should == all_posts }
+    end
+    context "no one is logged in" do 
+      Given(:published_posts) { [ stub_model(Post), stub_model(Post)] } 
+      Given { Post.stub(:published).and_return(published_posts) }
+      Given { subject.stub(:current_admin).and_return(nil) }
+      When { get :index }  
+      Then { response.code.should == "200" }
+      Then { response.should render_template(:index) }
+      Then { assigns(:posts).should == published_posts }
     end
   end
 
   describe "#show" do
-    it "successful request" do
-      slug = "my-title"
-      post = stub_model(Post, slug: slug)
-      Post.stub(:find_by_slug).and_return(post)
-      get :show, id: post.slug
-      response.code.should == "200"
-      assigns(:post).should == post
+    context "post exists" do
+      Given(:slug) { "my-title" }
+      Given(:target_post) { stub_model(Post, slug: slug) }
+      Given { Post.stub(:find_by_slug).and_return(target_post) }
+      When { get :show, id: target_post.slug }
+      Then { response.code.should == "200" }
+      Then { response.should render_template(:show) }
+      Then { assigns(:post).should == target_post }
     end
 
     context "post not found" do
