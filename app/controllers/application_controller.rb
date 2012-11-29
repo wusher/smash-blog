d2bc59803eab72398dbd23ff55e4445507f1ad6c
Will Usher <willusher+github@gmail.com>
2012-11-28 19:25:01 -0800

    remvoed ruby profiler for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :authorize_miniprofiler
   
-  private 
-  
-  def authorize_miniprofiler
-    if current_admin.present? || params[:profiler].present?
-      Rack::MiniProfiler.authorize_request
-    end
-  end
 end
