ed55100d1616c27ef602babef956abbd1e2a8b10
wusher <willusher+github@gmail.com>
2013-09-06 11:33:46 -0400

    cleanning up code white space

--- app/models/markdown_render.rb
+++ app/models/markdown_render.rb
@@ -1,8 +1,13 @@
 class MarkdownRender < Redcarpet::Render::HTML
 
-
   def block_code(code, language)
-    CodeRay.scan(code,language).div(css: :class)
+    CodeRay.scan(clean(code),language).div(css: :class)
+  end
+
+  private
+
+  def clean(code)
+    code.rstrip
   end
 
 end
--- spec/models/markdown_render_spec.rb
+++ spec/models/markdown_render_spec.rb
@@ -3,12 +3,31 @@ require 'spec_helper'
 describe MarkdownRender do
 
   describe "#block_code" do
-    Given(:code) { 'def square(x)\nx*x\nend' }
-    Given(:language) { "ruby" }
-    When(:html) { subject.block_code(code,language) }
-    Then { html.should == """<div class=\"CodeRay\">
-  <div class=\"code\"><pre><span class=\"keyword\">def</span> <span class=\"function\">square</span>(x)<span class=\"error\">\\</span>nx*x<span class=\"error\">\\</span>nend</pre></div>
+    context "normal code" do
+      Given(:code) { "def square(x)\nx*x\nend" }
+      Given(:language) { "ruby" }
+      When(:html) { subject.block_code(code,language) }
+      Then { html.should == <<-HTML
+<div class=\"CodeRay\">
+  <div class="code"><pre><span class="keyword">def</span> <span class="function">square</span>(x)
+x*x
+<span class="keyword">end</span></pre></div>
 </div>
-""" }
+HTML
+}
+    end
+    context "trailing white space" do
+      Given(:code) { "def square(x)\nx*x\nend" }
+      Given(:language) { "ruby" }
+      When(:html) { subject.block_code(code,language) }
+      Then { html.should == <<-HTML
+<div class=\"CodeRay\">
+  <div class="code"><pre><span class="keyword">def</span> <span class="function">square</span>(x)
+x*x
+<span class="keyword">end</span></pre></div>
+</div>
+HTML
+      }
+    end
   end
 end
