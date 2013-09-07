5804e87ba8835486ed3cacfc442140ca54a6b54c
wusher <willusher+github@gmail.com>
2013-09-07 15:54:26 -0400

    added indices to posts

---
+++ db/migrate/20130907194935_add_indices_to_posts.rb
@@ -0,0 +1,7 @@
+class AddIndicesToPosts < ActiveRecord::Migration
+  def change
+    add_index :posts, :slug
+    add_index :posts, :pubdate, order: { pubdate: "desc" }
+    add_index :posts, :published, order: { published: "desc" } 
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,37 +9,41 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121008043751) do
+ActiveRecord::Schema.define(version: 20130907194935) do
 
-  create_table "admins", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+  create_table "admins", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
-  add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
-  add_index "admins", ["reset_password_token"], :name => "index_admins_on_reset_password_token", :unique => true
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
+  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
 
-  create_table "posts", :force => true do |t|
+  create_table "posts", force: true do |t|
     t.string   "title"
     t.string   "slug"
     t.text     "body"
     t.text     "body_html"
     t.date     "pubdate"
     t.boolean  "published"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
+  add_index "posts", ["pubdate"], name: "index_posts_on_pubdate"
+  add_index "posts", ["published"], name: "index_posts_on_published"
+  add_index "posts", ["slug"], name: "index_posts_on_slug"
 
 end
